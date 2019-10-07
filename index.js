b866d10abb5ddcf58483faf0b803077c7ae08ce5
Nik Coughlin <nrkn.com@gmail.com>
2019-10-08 12:41:19 +1300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const { executeFast } = require( './execute-fast' )
 const { parse } = require( './parse' )
 
 const hrToMs = ( [ s, ns ] ) => s * 1e3 + ns / 1e6
-
 const mips = ( ms, ticks ) => ( 1e6 / ( ms / ticks ) ) / 1e6
+const logMemory = memory => console.log( 'memory', Array.from( memory ) )
 
 const memory = new Uint32Array( 3 )
 const times = 9e4
+
+const program = [
+  // 0 - i
+  'set 0 0',
+  // 1 - length
+  `set 1 ${ times }`,
+  // 2 - result
+  'set 2 0',
+
+  // :loop
+  // 3 - result += i
+  'add 2 0',
+  // 4 - i++
+  'vadd 0 1',
+  // 5 - length--
+  'vsub 1 1',
+  // 6 - if length === 0
+  'vjmpz 1 8',
+  // 7 - else
+  'vjmp 3',
+  // 8 - break
+  'brk'
+]
 
 const startNative = process.hrtime()
 
@@ -33,31 +56,6 @@ console.log(
 )
 console.log( 'result', result )
 
-const program = [
-  // 0 - i
-  'set 0 0',
-  // 1 - length
-  `set 1 ${ times }`,
-  // 2 - result
-  'set 2 0',
-
-  // :loop
-  // 3 - result += i
-  'add 2 0',
-  // 4 - i++
-  'vadd 0 1',
-  // 5 - length--
-  'vsub 1 1',
-  // 6 - if length === 0
-  'vjmpz 1 8',
-  // 7 - else
-  'vjmp 3',
-  // 8 - break
-  'brk'
-]
-
-const logMemory = () => console.log( 'memory', Array.from( memory ) )
-
 const compareNative = ( name, fn ) => {
   memory.fill( 0 )
 
@@ -71,7 +69,7 @@ const compareNative = ( name, fn ) => {
   console.log( 'ratio:native', ( end / endNative ).toFixed( 2 ) )
   console.log( 'ticks', ticks.toLocaleString() )
   console.log( 'MIPS', Math.floor( mips( end, ticks ) ).toLocaleString() )
-  logMemory()
+  logMemory( memory )
 }
 
 const instructions = program.map( parse )
