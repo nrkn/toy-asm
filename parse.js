6eb1f54dc55ee72d5c84d44928f9ccfc57bbe020
Nik Coughlin <nrkn.com@gmail.com>
2019-10-08 16:58:14 +1300

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,27 +1,30 @@
 const parse = instruction => {
   let [ op, arg1, arg2 ] = instruction.split( /\s+/ ).filter( s => s !== '' )
 
-  if( arg1 === undefined ) return [ op ]
-
-  if( arg1.startsWith( '$' ) ){
-    arg1 = Number( arg1.substring( 1 ) )
-  } else {
-    arg1 = Number( arg1 )
-    op = 'v' + op
+  if( arg1 === undefined ){
+    return [ op ]
   }
+
+  [ op, arg1 ] = parseArg( op, arg1 )
 
   if( arg2 === undefined ){
     return [ op, arg1 ]
   }
 
-  if( arg2.startsWith( '$' ) ){
-    arg2 = Number( arg2.substring( 1 ) )
-  } else {
-    arg2 = Number( arg2 )
-    op = 'v' + op
-  }
+  [ op, arg2 ] = parseArg( op, arg2 )
 
   return [ op, arg1, arg2 ]
+}
+
+const parseArg = ( op, arg ) => {
+  if( arg.startsWith( '$' ) ){
+    arg = Number( arg.substring( 1 ) )
+  } else {
+    arg = Number( arg )
+    if( !op.startsWith( 'v' ) ) op = 'v' + op
+  }
+
+  return [ op, arg ]
 }
 
 module.exports = { parse }
